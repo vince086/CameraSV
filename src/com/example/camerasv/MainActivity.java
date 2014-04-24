c0055a82a29a5ebe69dccb3e3ec23df5a8508efd
vince086 <vincentyegen1@gmail.com>
2014-04-24 11:29:03 +0100

    Starting point for App. Layout set but no working code for button or camera

--- src/com/example/camerasv/MainActivity.java
+++ src/com/example/camerasv/MainActivity.java
@@ -18,11 +18,19 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
+    public boolean onCreateOptionsMenu(Menu menu) {   
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
